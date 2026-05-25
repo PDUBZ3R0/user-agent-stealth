cf1107d5a0e169cd403071cadeb8d02e290ab408
pdubz3r0 <pdubz3r0@proton.me>
2026-05-24 22:23:58 -0400

    Bugfixes for new Version

--- update/consts.js
+++ update/consts.js
@@ -4,50 +4,52 @@ export const MIN_VERSION_SHARE = 1;
 export const DEFAULTS = {
   chromeDesktop: {
     distribution: {
-      '147': 80,
-      '148': 20,
+      '147': 15,
+      '148': 50,
+      '149': 35,
     },
   },
   chromeAndroid: {
     distribution: {
-      '147': 36,
-      '148': 64,
+      '147': 11,
+      '148': 7,
+      '149': 82,
     },
   },
   chromeIOS: {
     distribution: {
-      '146': 67,
-      '147': 18,
-      '148': 15,
+      '147': 9,
+      '148': 46,
+      '149': 45,
     },
   },
   safariMacOS: {
     distribution: {
-      '26.5': 79,
-      '26.4': 21,
+      '26.5': 76,
+      '26.4': 24,
     },
   },
   safariIOS: {
     distribution: {
-      '26.5': 22,
-      '26.4': 78,
+      '26.5': 23,
+      '26.4': 77,
     },
   },
   brave: {
     distribution: {
-      '1.90': 88,
-      '1.89': 3,
-      '1.88': 9,
+      '1.90': 73,
+      '1.89': 22,
+      '1.88': 5,
     },
   },
   androidWebView: {
     distribution: {
-      '147': 4,
-      '148': 96,
+      '147': 31,
+      '148': 66,
+      '149': 3,
     },
   },
 };
 
 export const DESKTOP_AVAILABILITY = {"windows":["Chrome","Firefox","Edge","Opera","Brave","Vivaldi","Yandex Browser","IE"],"macos":["Safari","Chrome","Firefox","Edge","Opera","Brave","Vivaldi"],"linux":["Chrome","Firefox","Chromium","Edge","Opera","Brave","Vivaldi"],"chromeos":["Chrome"]};
-
 export const MOBILE_AVAILABILITY = {"android":["Chrome","Samsung Internet","Opera","Firefox","UC Browser","Edge","Android","Yandex Browser","Brave"],"ios":["Safari","Chrome","Firefox","Edge","Opera"]};
